65096de5f3f22395bbb1b3f446f34ef1f919e40a
Daiane Gonçalves <daianeecs@icloud.com>
2022-02-15 09:20:28 -0300

    correções no index

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,42 @@
-function encode(suaSenha, offset, escolhaFun) {
-  let asciiCode = 0;
-  let senhaFinal = "";
-
-  function caractere (n, m) {
-    return ((n % m) + m) % m;
-  }   
+const cipher = {
   
-  if (escolhaFun == true) {
-    offset = offset * -1;
-  }
-  for (let letr of suaSenha) {
-    if (letr.charCodeAt() == 32) {
-      senhaFinal += "";
-    }
-    else if (letr.charCodeAt() >= 65 && letr.charCodeAt() <= 90) {
-      asciiCode = caractere((letr.charCodeAt() - 65 + offset), 26) + 65;
-      senhaFinal += String.fromCharCode(asciiCode);
-    }
-    else if (letr.charCodeAt() >= 97 && letr.charCodeAt() <= 122) {
-      asciiCode = caractere((letr.charCodeAt() - 97 + offset), 26) + 97;
-      senhaFinal += String.fromCharCode(asciiCode);
-    }
-    else {
-      senhaFinal += letr;
-    }
-  }
-  let result = document.getElementById("input-result");
-  result.value = senhaFinal;
-  return senhaFinal;
-}
+    encode(offset, string) {
+      if (offset == 0 || offset == null || offset == undefined) {
+        throw new TypeError();
+      }
 
-export default encode;
+        let senhaCode = "";
+        let quantidadeLetras = string.length;
+
+        for (let i = 0; i < quantidadeLetras; i++) {
+            let valorAsc = string.charCodeAt(i)
+
+            let codigoLetra = 65;
+            let valorEncode = ((valorAsc - codigoLetra + offset) % 26) + codigoLetra;
+            senhaCode = senhaCode.concat(String.fromCharCode(valorEncode));
+        }
+
+        return senhaCode;
+    },
+
+    decode(offset, string) {
+
+      if (offset == 0 || offset == null || offset == undefined) {
+        throw new TypeError();
+      }
+      
+      let senhaDecode = "";
+      let quantidadeLetras = string.length;
+      for (let i = 0; i < quantidadeLetras; i++) {
+          let valorAsc = string.charCodeAt(i)
+
+          let codigoLetra = 90;
+          let valorDecode = ((valorAsc - codigoLetra - offset) % 26) + codigoLetra;
+          senhaDecode = senhaDecode.concat(String.fromCharCode(valorDecode));
+      }
+      return senhaDecode;
+  }
+
+};
+
+export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import encode from "./cipher.js";
+import cipher from "./cipher.js";
 
-document.getElementById("btncifrar").addEventListener("click", () => {
-  let suaSenha = document.getElementById("input-senha").value;
-  let offset = parseInt(document.getElementById("key").value);
-  let escolhaFun = document.getElementById("rddecode").checked;
-  encode(suaSenha, offset, escolhaFun);
-})
+document.getElementById("btncifrar").addEventListener("click", cifrarSenha)
+function cifrarSenha() {
 
-const copyBtn = document.getElementById("copybtn").addEventListener("click", () => {
-  let suaSenha = document.getElementById("input-result");
-  suaSenha.select();
-  document.execCommand("copy");
-})
+    let suaSenha = document.getElementById("suaSenha").value.toUpperCase();
+    let offset = document.getElementById("offset").value;
+    let senhaEncode = cipher.encode(offset, suaSenha);
+    document.getElementById("senhaPronta").value = senhaEncode;
+
+}
+
+document.getElementById("btndecifrar").addEventListener("click", decifrarSenha);
+function decifrarSenha() {
+
+    let suaSenha = document.getElementById("suaSenha").value.toUpperCase();
+    let offset = document.getElementById("offset").value;
+    let senhaDecode = cipher.decode(offset, suaSenha);
+    document.getElementById("senhaPronta").value = senhaDecode;
+
+
+}
